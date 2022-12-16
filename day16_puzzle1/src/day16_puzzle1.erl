3653d3e97e6e73e2af85f0f0fcee053a6da2b6e7
Aleksander Lisiecki <alek.lisiecki@gmail.com>
2022-12-16 12:31:40 +0100

    Solve day 16 puzzle 1

--- day16_puzzle1/src/day16_puzzle1.erl
+++ day16_puzzle1/src/day16_puzzle1.erl
@@ -11,10 +11,7 @@
 main(Args) ->
     {ok, RawBinaryInput} = file:read_file(hd(Args)),
     TunnelsWithValves = parse_data(RawBinaryInput),
-
-    io:format("TunnelsWithValves: ~p~n", [TunnelsWithValves]),
-    % Answer = dfs_all_possibilities("AA", sets:new(), #{}, 30, TunnelsWithValves),
-    Answer = dfs_all_possibilities("AA", [], #{}, 30, TunnelsWithValves),
+    Answer = find_answer(TunnelsWithValves, #{filter_after => 1, hard_max_considered => 10}),
     io:format("Answer: ~p~n", [Answer]),
     erlang:halt(0).
 
@@ -22,48 +19,145 @@ main(Args) ->
 %% Internal functions
 %%====================================================================
 
-dfs_all_possibilities(CurrentCorridor, OpenedValves, ValvesWithOpeningTimes, 1, TunnelsWithValves) ->
-    lists:sum(lists:map(fun(OpenedValve) ->
-        OpenTime = maps:get(OpenedValve, ValvesWithOpeningTimes),
-        {ValveFlowRate, _NextSteps} = maps:get(CurrentCorridor, TunnelsWithValves),
-        OpenTime * ValveFlowRate
-    end, OpenedValves));
-dfs_all_possibilities(CurrentCorridor, OpenedValves, ValvesWithOpeningTimes, MinutesLeft, TunnelsWithValves) ->
-    {ValveFlowRate, NextCorridors} = maps:get(CurrentCorridor, TunnelsWithValves),
-    
-    NextStepsCalculated = lists:map(fun(NewCorridor) ->
-            dfs_all_possibilities(NewCorridor, OpenedValves,
-                ValvesWithOpeningTimes, MinutesLeft - 1, TunnelsWithValves)
-        end, NextCorridors),
-    % case sets:is_element(CurrentCorridor, OpenedValves) of
-    case lists:member(CurrentCorridor, OpenedValves) orelse ValveFlowRate == 0 of
-        true -> lists:max(NextStepsCalculated);
-        false -> 
-            lists:max([
-                dfs_all_possibilities(
-                    % CurrentCorridor, sets:add_element(CurrentCorridor, OpenedValves),
-                    CurrentCorridor, [CurrentCorridor | OpenedValves],
-                    maps:put(CurrentCorridor, MinutesLeft, ValvesWithOpeningTimes),
-                    MinutesLeft - 1, TunnelsWithValves)
-                | NextStepsCalculated
-            ])
+find_answer(TunnelsWithValves, FilteringOptions) ->
+    ValvesThatMatter = find_valves_with_flow(TunnelsWithValves),
+    PathsOfActions =
+        find_answer(TunnelsWithValves, FilteringOptions, [{[{open, "AA"}], ValvesThatMatter}]),
+    lists:max(
+        lists:map(fun(PathOfActions) -> count_released_pressure(PathOfActions, TunnelsWithValves)
+                  end,
+                  PathsOfActions)).
+
+find_answer(_TunnelsWithValves, _, Paths = [{_, []} | _]) ->
+    lists:map(fun({Path, _}) -> Path end, Paths);
+find_answer(TunnelsWithValves,
+            FilteringOptions = #{filter_after := FilterLeastPromisingAfter},
+            Paths) ->
+    io:format("FilterLeastPromisingAfter: ~p~n", [FilterLeastPromisingAfter]),
+    AllNewPaths =
+        lists:flatten([[{begin
+                             {_, First} = lists:last(Path),
+                             [_ | NewStepByStepPath] =
+                                 concept_path_to_step_by_step_path([First, Valve],
+                                                                   TunnelsWithValves),
+                             Path ++ NewStepByStepPath
+                         end,
+                         UnusedValvesThatMatter -- [Valve]}
+                        || Valve <- UnusedValvesThatMatter]
+                       || {Path, UnusedValvesThatMatter} <- Paths]),
+    SomeNewPaths =
+        case FilterLeastPromisingAfter =< 0 of
+            true ->
+                filter_least_promising_paths(AllNewPaths, FilteringOptions, TunnelsWithValves);
+            false ->
+                AllNewPaths
+        end,
+    find_answer(TunnelsWithValves,
+                FilteringOptions#{filter_after => FilterLeastPromisingAfter - 1},
+                SomeNewPaths).
+
+filter_least_promising_paths(AllNewPaths,
+                             #{hard_max_considered := HardMax},
+                             TunnelsWithValves) ->
+    AllNewPathsMeasured =
+        lists:map(fun({Path, Sth}) ->
+                     {Path, Sth, count_released_pressure(Path, TunnelsWithValves)}
+                  end,
+                  AllNewPaths),
+    SortedPaths = lists:sort(fun({_, _, A}, {_, _, B}) -> A > B end, AllNewPathsMeasured),
+    {PromisingPaths, _DropOuts} = lists:split(HardMax, SortedPaths),
+    lists:map(fun({Path, UnvisitedValves, _}) -> {Path, UnvisitedValves} end, PromisingPaths).
+
+concept_path_to_step_by_step_path(ConceptPath, TunnelsWithValves) ->
+    ConceptSteps = lists:zip([start | ConceptPath], ConceptPath ++ [finish]),
+    [_ | Steps] =
+        lists:flatten(
+            lists:map(fun(FromTo) -> concept_step_to_path(FromTo, TunnelsWithValves) end,
+                      ConceptSteps)),
+    Steps.
+
+find_valves_with_flow(TunnelsWithValves) ->
+    maps:keys(
+        maps:filter(fun(_Corridor, {Flow, _}) -> Flow > 0 end, TunnelsWithValves)).
+
+% perms([]) -> [[]];
+% perms(L) -> [[H|T] || H <- L, T <- perms(L--[H])].
+
+concept_step_to_path({start, To}, _TunnelsWithValves) ->
+    [{move, To}];
+concept_step_to_path({From, finish}, _TunnelsWithValves) ->
+    [{open, From}];
+concept_step_to_path({From, To}, TunnelsWithValves) ->
+    lists:map(fun (Elem) when Elem == From ->
+                      {open, Elem};
+                  (Elem) ->
+                      {move, Elem}
+              end,
+              lists:reverse(shortest_path(From, To, TunnelsWithValves))).
+
+shortest_path(From, To, TunnelsWithValves) ->
+    case find_shortest_path(To, TunnelsWithValves, [{[From]}]) of
+        [{Path}] ->
+            Path;
+        [{Path} | _] ->
+            Path
+    end.
+
+find_shortest_path(To, TunnelsWithValves, Paths) ->
+    case lists:any(fun({Path}) -> hd(Path) == To end, Paths) of
+        true ->
+            lists:filter(fun({Path}) -> hd(Path) == To end, Paths);
+        false ->
+            NewPaths =
+                lists:flatten(
+                    lists:map(fun({Path}) ->
+                                 {_, NextSteps} = maps:get(hd(Path), TunnelsWithValves),
+                                 [{[NextStep | Path]} || NextStep <- NextSteps]
+                              end,
+                              Paths)),
+            find_shortest_path(To, TunnelsWithValves, NewPaths)
+    end.
+
+count_released_pressure(PathsOfActions, TunnelsWithValves) ->
+    count_released_pressure(PathsOfActions, 30, TunnelsWithValves, []).
+
+count_released_pressure(_PathsOfActions, 1, TunnelsWithValves, OpenedValvesWithTime) ->
+    lists:sum(
+        lists:map(fun({Valve, Time}) ->
+                     {Flow, _} = maps:get(Valve, TunnelsWithValves),
+                     Time * Flow
+                  end,
+                  OpenedValvesWithTime));
+count_released_pressure([], _TimeLeft, TunnelsWithValves, OpenedValvesWithTime) ->
+    count_released_pressure([], 1, TunnelsWithValves, OpenedValvesWithTime);
+count_released_pressure([CurrentAction | NextActions],
+                        TimeLeft,
+                        TunnelsWithValves,
+                        OpenedValvesWithTime) ->
+    case CurrentAction of
+        {open, Valve} ->
+            count_released_pressure(NextActions,
+                                    TimeLeft - 1,
+                                    TunnelsWithValves,
+                                    [{Valve, TimeLeft} | OpenedValvesWithTime]);
+        {move, _Valve} ->
+            count_released_pressure(NextActions,
+                                    TimeLeft - 1,
+                                    TunnelsWithValves,
+                                    OpenedValvesWithTime)
     end.
 
 parse_data(RawBinaryInput) ->
     RawLstInput = binary:bin_to_list(RawBinaryInput),
     RawValvesList = string:split(RawLstInput, "\n", all),
-    maps:from_list(lists:map(fun parse_valve/1, RawValvesList)).
+    maps:from_list(
+        lists:map(fun parse_valve/1, RawValvesList)).
 
 % Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
 parse_valve(RawValve) ->
     [RawNameAndFlowRate, RawTunnels] = string:split(RawValve, ";"),
-    {
-        get_valve_name(RawNameAndFlowRate),
-        {
-            get_valve_flow_rate(RawNameAndFlowRate),
-            get_tunnels(RawTunnels)
-        }
-    }.
+    {get_valve_name(RawNameAndFlowRate),
+     {get_valve_flow_rate(RawNameAndFlowRate), get_tunnels(RawTunnels)}}.
 
 get_valve_name(RawNameAndFlowRate) ->
     [_, ValveName | _] = string:split(RawNameAndFlowRate, " ", all),
