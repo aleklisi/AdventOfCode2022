b67a78773caa2c30224f8310e1da64ac8335c757
Aleksander Lisiecki <sebaczechu123@gmail.com>
2022-12-17 15:08:48 +0100

    Flaw solution day 17 puzzle 2

--- day17_puzzle2/src/day17_puzzle2.erl
+++ day17_puzzle2/src/day17_puzzle2.erl
@@ -1,47 +1,79 @@
 -module(day17_puzzle2).
 
+-include_lib("eunit/include/eunit.hrl").
+
 %% API exports
 -export([main/1]).
 
 -define(BOARD_SIZE, 7).
--define(MAX_ROCKS_COUNT, 1_000_000).
-% -define(MAX_ROCKS_COUNT, 1_000_000_000_000).
 -define(ABOVE, 4).
-% -define(RESET_EVERY, infinity). % Execution time for 10_000 rocks 4739.481 ms
--define(RESET_EVERY, 20).         % Execution time for 10_000 rocks  188.019 ms
+-define(ETS_HISTORY_TABLE_NAME, dropped_shapes_history_table).
 
 %% escript Entry point
-main(Args) ->
-    {ok, RawBinaryInput} = file:read_file(hd(Args)),
+main([Filename, RawMaxRocksCount]) ->
+    {ok, RawBinaryInput} = file:read_file(Filename),
+    {MaxRocksCount, ""} = string:to_integer(RawMaxRocksCount),
     Moves = parse_data(RawBinaryInput),
     InitialBoard = sets:from_list(shape(floor, ok)),
-    {Time, {FinalBoard, _History}} = timer:tc(fun() -> run_simulation(InitialBoard, Moves, ?MAX_ROCKS_COUNT) end),
+    
+    % Start history
+    ets:new(?ETS_HISTORY_TABLE_NAME, [named_table, bag]),
+
+    {Time, FinalBoard} =
+        timer:tc(fun() -> run_simulation(InitialBoard, Moves, MaxRocksCount) end),
     Answer = find_height(FinalBoard),
+
     io:format("SimulationTime: ~p ms~n", [Time / 1000]),
     io:format("Answer: ~p~n", [Answer]),
-    erlang:halt(0).
+    
+    % Clean up history
+    ets:delete(?ETS_HISTORY_TABLE_NAME),
+    Answer.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
 run_simulation(InitialBoard, Moves, MaxRocksCount) ->
-    run_simulation(InitialBoard, Moves, 1, 1, 0, MaxRocksCount, []).
+    run_simulation(InitialBoard, Moves, 1, 1, 0, MaxRocksCount, true).
 
-run_simulation(Board, _Moves, _ShapeNum, _MoveNum, RocksCount, MaxRocksCount, History) when RocksCount == MaxRocksCount ->
-    {Board, History};
-run_simulation(_Board, Moves, ShapeNum, MoveNum, RocksCount, MaxRocksCount, History) when length(History) == ?RESET_EVERY ->
-    NewBoard = sets:from_list(lists:flatten([shape(floor, ok) | History])),
-    run_simulation(NewBoard, Moves, ShapeNum, MoveNum, RocksCount, MaxRocksCount, []);
-run_simulation(Board, Moves, ShapeNum, MoveNum, RocksCount, MaxRocksCount, History) when ShapeNum == 6 ->
-    run_simulation(Board, Moves, 1, MoveNum, RocksCount, MaxRocksCount, History);
-run_simulation(Board, Moves, ShapeNum, MoveNum, RocksCount, MaxRocksCount, History) ->
+run_simulation(Board, _Moves, _ShapeNum, _MoveNum, RocksCount, MaxRocksCount, _) when RocksCount >= MaxRocksCount -> Board;
+run_simulation(Board, Moves, ShapeNum, MoveNum, RocksCount, MaxRocksCount, SearchPattern) when ShapeNum == 6 ->
+    run_simulation(Board, Moves, 1, MoveNum, RocksCount, MaxRocksCount, SearchPattern);
+run_simulation(Board, Moves, ShapeNum, MoveNum, RocksCount, MaxRocksCount, SearchPattern) ->
     NewShapesY = find_height(Board),
     NewShape = shape(ShapeNum, NewShapesY),
-    {NewMoveNum, DroppedShape} = drop_shape(NewShape, Board, Moves, MoveNum),
+    {NewMoveNum, [{X, _Y} | _] = DroppedShape} = drop_shape(NewShape, Board, Moves, MoveNum),
+    % Adding new squares to the board
     NewBoard = sets:union(Board, sets:from_list(DroppedShape)),
-    NewHistory = [DroppedShape | History],
-    run_simulation(NewBoard, Moves, ShapeNum + 1, NewMoveNum, RocksCount + 1, MaxRocksCount, NewHistory).
+    % SearchPattern is a boolean variable to determine if I already jumped or not,
+    % if I did not then I save my shapes in ETS table to be matched against
+    % else I assume I jumped already and now I just need to run the simulation like in the puzzle 1 
+    case SearchPattern of
+        true ->
+            CurrentHeight = find_height(Board),
+            NewOccurrence = {{ShapeNum, MoveNum, RocksCount, find_height(Board)}, DroppedShape},
+            case ets:match_object(?ETS_HISTORY_TABLE_NAME, {{ShapeNum, MoveNum, '_', '_'}, [{X, '_'} | '_']}) of
+                [] ->
+                    true = ets:insert(?ETS_HISTORY_TABLE_NAME, NewOccurrence),
+                    run_simulation(NewBoard, Moves, ShapeNum + 1, NewMoveNum, RocksCount + 1, MaxRocksCount, SearchPattern);
+                [{{_, _, PreviousRockCount, PreviousHeight}, _}] ->
+                    PatternEverNRocks = RocksCount - PreviousRockCount,
+                    PatternHeight = CurrentHeight - PreviousHeight,
+                    jump(Board, PatternEverNRocks, PatternHeight, Moves, ShapeNum, MoveNum, RocksCount, MaxRocksCount)
+            end;
+        false ->
+            run_simulation(NewBoard, Moves, ShapeNum + 1, NewMoveNum, RocksCount + 1, MaxRocksCount, SearchPattern)
+    end.
+
+jump(Board, PatternEverNRocks, PatternHeight, Moves, ShapeNum, MoveNum, RocksCount, MaxRocksCount) ->
+    HowMayFullPatters = (MaxRocksCount - RocksCount) div PatternEverNRocks,
+    HowManyRocksLeft = (MaxRocksCount - RocksCount) rem PatternEverNRocks,
+    NewBoard =
+        sets:from_list(
+            lists:map(fun({X, Y}) -> {X, Y + PatternHeight * HowMayFullPatters} end,
+                      sets:to_list(Board))),
+    run_simulation(NewBoard, Moves, ShapeNum, MoveNum, 0, HowManyRocksLeft, false).
 
 drop_shape(Shape, Board, Moves, MoveNum) when MoveNum == length(Moves) + 1 ->
     drop_shape(Shape, Board, Moves, 1);
@@ -127,20 +159,22 @@ shape(3, CurrentMaxY) ->
 shape(4, CurrentMaxY) ->
     X = 3,
     Y = CurrentMaxY + ?ABOVE,
-    [
-        {X, Y + 3},
-        {X, Y + 2},
-        {X, Y + 1},
-        {X, Y}
-    ];
+    [{X, Y + 3}, {X, Y + 2}, {X, Y + 1}, {X, Y}];
 % ##
 % ##
 shape(5, CurrentMaxY) ->
     X = 3,
     Y = CurrentMaxY + ?ABOVE,
-    [
-        {X, Y + 1}, {X + 1, Y + 1},
-        {X, Y}, {X + 1, Y}
-    ];
+    [{X, Y + 1}, {X + 1, Y + 1}, {X, Y}, {X + 1, Y}];
 shape(floor, _) ->
     [{X, 0} || X <- lists:seq(1, 7)].
+
+
+small_example_test() ->
+    ?assertEqual(3068, main(["./data/puzzle_17_test_input", "2022"])).
+
+small_task_test() ->
+    ?assertEqual(3149, main(["./data/puzzle_17_input", "2022"])).
+
+big_example_test() ->
+    ?assertEqual(1514285714288, main(["./data/puzzle_17_test_input", "1000000000000"])).
